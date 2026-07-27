c3b6ffea705f1a73879935670d57c154f4d2289b
cnvt1803 <vantruongasds@gmail.com>
2026-07-27 11:05:19 +0700

    fix ws domain

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3855895200"
+    serviceWorkerVersion: "1398016042"
   }
 });
