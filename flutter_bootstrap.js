22f57dd10cd8de0e34e0c13719c731f4fe032028
cnvt1803 <vantruongasds@gmail.com>
2026-07-27 17:22:29 +0700

    fix some issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1044460370"
+    serviceWorkerVersion: "3301935639"
   }
 });
