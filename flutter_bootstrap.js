5a4022bbef7fb76825f68184e6b8c99eba476605
cnvt1803 <vantruongasds@gmail.com>
2026-09-04 10:24:14 +0700

    [OTS web app] - update 26093

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1080065980"
+    serviceWorkerVersion: "858422438"
   }
 });
